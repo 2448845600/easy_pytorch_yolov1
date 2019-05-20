003314a0feda90d2c1280116f01f1c0a4d0412dc
hansmile <2448845600@qq.com>
2019-05-20 14:23:18 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+## IOU
+[![EOF7vt.png](https://s2.ax1x.com/2019/05/18/EOF7vt.png)](https://imgchr.com/i/EOF7vt)
+
+由于坐标轴的设定，Y轴数值向下为增加，所以
+$$
+intersection = [min(y2, y2') - max(y1, y1')] * [min(x2, x2') - max(x1, x1')]
+$$
+
+## loss
+
+
+$$
+\begin{split}
+loss &= \lambda_{\text { coord }} \sum_{i=0}^{S^{2}} \sum_{j=0}^{B} \mathbb{1}_{i j}^{\text { obj }}\left[\left(x_{i}-\hat{x}_{i}\right)^{2}+\left(y_{i}-\hat{y}_{i}\right)^{2}\right]\\
+
+&+\lambda_{\text { coord }} \sum_{i=0}^{S^{2}} \sum_{j=0}^{B} \mathbb{1}_{i j}^{\text { obj } j}\left[\left(\sqrt{w_{i}}-\sqrt{\hat{w}_{i}}\right)^{2}+\left(\sqrt{h_{i}}-\sqrt{\hat{h}_{i}}\right)^{2}\right]\\
+
+&+\sum_{i=0}^{S^{2}} \sum_{j=0}^{B} \mathbb{1}_{i j}^{\mathrm{obj}}\left(C_{i}-\hat{C}_{i}\right)^{2}\\
+
+&+\lambda_{\mathrm{noobj}} \sum_{i=0}^{S^{2}} \sum_{j=0}^{B} \mathbb{1}_{i j}^{\mathrm{noobj}}\left(C_{i}-\hat{C}_{i}\right)^{2}\\
+
+&+\sum_{i=0}^{S^{2}} \mathbb{1}_{i}^{\mathrm{obj}} \sum_{c \in \text { classes }}\left(p_{i}(c)-\hat{p}_{i}(c)\right)^{2}\\
+
+\end{split}
+$$
+
+loss.py文件把loss分成几部分，则有四种情况：
+
+1 target存在框，prediction没有框：计算not response loss 未响应损失
+2 target存在框，prediction存在框：计算response loss响应损失
+3 target没有框，prediction没有框：无损失(不计算)
+4 target没有框，prediction存在框：计算不包含obj损失  只计算confidence loss
+
+## Data
+voc的数据格式是xml，转换为txt，转换代码在data目录下。定义YOLODataset。
+
+
+
---
+++ main.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import numpy as np
+import cv2
+
+from model.yolo import YOLO
+from util.visualize import Visualizer
+from config import conf
+from data.dataset import YOLODataset
+from model.loss import YOLOLoss
+
+
+def train():
+    vis = Visualizer(conf.env)
+
+    print("==============================\n网络结构  开始 \n==============================")
+    yolo = YOLO()
+    print("----------\n网络结构  加载预训练模型\n----------")
+    if conf.load_model_path:
+        yolo.load_state_dict(torch.load(conf.load_model_path, map_location=lambda storage, loc: storage))
+    if conf.use_gpu:
+        yolo.cuda()
+    print("----------\n网络结构  输出网络\n----------")
+    print(yolo)
+    yolo.train()
+    print("==============================\n网络结构  结束 \n==============================")
+
+    print("==============================\n加载数据  开始 \n==============================")
+    train_dataset = YOLODataset(root=conf.file_root, list_file=conf.voc_2012train, train=True,
+                                transform=[transforms.ToTensor()])
+    train_loader = DataLoader(train_dataset, batch_size=conf.batch_size, shuffle=True, num_workers=conf.num_workers)
+    test_dataset = YOLODataset(root=conf.test_root, list_file=conf.voc_2007test, train=False,
+                               transform=[transforms.ToTensor()])
+    test_loader = DataLoader(test_dataset, batch_size=conf.batch_size, shuffle=False, num_workers=conf.num_workers)
+    print("==============================\n加载数据  结束 \n==============================")
+
+    criterion = YOLOLoss(7, 2, 5, 0.5)
+    optimizer = torch.optim.SGD(yolo.parameters(), lr=conf.learning_rate, momentum=conf.momentum,
+                                weight_decay=conf.weight_decay)
+    logfile = open('log/log.txt', 'w')
+    best_test_loss = np.inf
+    print('训练集有 %d 张图像' % (len(train_dataset)))
+    print('一个batch的大小为 %d' % (conf.batch_size))
+
+    print("==============================\n训练  开始 \n==============================")
+    for epoch in range(conf.num_epochs):
+        if epoch == 1:
+            conf.learning_rate = 0.0005
+        if epoch == 2:
+            conf.learning_rate = 0.00075
+        if epoch == 3:
+            conf.learning_rate = 0.001
+        if epoch == 80:
+            conf.learning_rate = 0.0001
+        if epoch == 100:
+            conf.learning_rate = 0.00001
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = conf.learning_rate
+            # 第几次epoch  及 当前epoch的学习率
+        print('\n\n当前的epoch为 %d / %d' % (epoch + 1, conf.num_epochs))
+        print('当前epoch的学习率: {}'.format(conf.learning_rate))
+
+        total_loss = 0.
+        for i, (images, target) in enumerate(train_loader):
+            images = Variable(images)
+            target = Variable(target)
+            if conf.use_gpu:
+                images, target = images.cuda(), target.cuda()
+            pred = yolo(images)
+            loss = criterion(pred, target)
+            total_loss += loss.data[0]
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i + 1) % conf.print_freq == 0:
+                print('在训练集上：当前epoch为 [%d/%d], Iter [%d/%d] 当前batch损失为: %.4f, 当前epoch到目前为止平均损失为: %.4f'
+                      % (epoch + 1, conf.num_epochs, i + 1, len(train_loader), loss.data[0], total_loss / (i + 1)))
+                # 画出训练集的平均损失
+                vis.plot_train_val(loss_train=total_loss / (i + 1))
+        torch.save(yolo.state_dict(), conf.current_epoch_model_path)
+
+        print("----------\n训练  对当前train做验证\n----------")
+        validation_loss = 0.0
+        yolo.eval()
+        for i, (images, target) in enumerate(test_loader):
+            images = Variable(images, volatile=True)  # volatile标记. 只能通过keyword传入.
+            target = Variable(target, volatile=True)
+            if conf.use_gpu:
+                images, target = images.cuda(), target.cuda()
+            pred = yolo(images)
+            loss = criterion(pred, target)
+            validation_loss += loss.data[0]
+        validation_loss /= len(test_loader)
+        vis.plot_train_val(loss_val=validation_loss)
+        if best_test_loss > validation_loss:
+            best_test_loss = validation_loss
+            print('当前得到最好的验证集的平均损失为  %.5f' % best_test_loss)
+            torch.save(yolo.state_dict(), conf.best_test_loss_model_path)
+
+        logfile.writelines(str(epoch) + '\t' + str(validation_loss) + '\n')
+        logfile.flush()
+
+
+def predict():
+    pass
+
+
+def eval():
+    pass
+
+
+if __name__ == '__main__':
+    import fire
+
+    fire.Fire()
+
+    train()
